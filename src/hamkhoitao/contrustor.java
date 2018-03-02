59f3bd87a8b3aa5bf2a2dfd094bc6a2540e355ca
admin <admin@DESKTOP-F2AJEQA>
2018-03-02 21:14:00 +0700

    sdf

---
+++ src/hamkhoitao/contrustor.java
@@ -0,0 +1,16 @@
+package hamkhoitao;
+
+public class contrustor {
+	int subid;
+	String sSubjectName;
+
+	// day la ham khoi tao khong tham so
+	public contrustor(){
+	}
+	// day la ham khoi tao co tham so
+	public contrustor(int ID , String sname) {
+		subid = ID;
+		sSubjectName = sname;
+	}
+
+}
---
+++ src/hamkhoitao/vd1.java
@@ -0,0 +1,15 @@
+package hamkhoitao;
+
+public class vd1 {
+	 vd1(){
+		this(100);
+	}
+	 vd1(int iteacher){
+		System.out.println(" idteacher:" + iteacher );
+	}
+	public static void main(String[] args) {
+		//khai bao va khoi tao doi tuong
+		vd1 teacher = new vd1();
+		
+	}
+}
